3415b699fd87116cf6572f64d962e9e4c2d6144f
ddrieck <ddrieck@gmail.com>
2014-02-06 15:49:47 -0500

    Finished jquery toggle of staff images.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
-	$('.visible').hover(function(){
-		$(this).slideUp();
-		$(this).next().show();
+	$('.thumbnail').mouseenter(function() {
+	    $(this).find(".hidden_action").fadeToggle();
+	    $(this).find(".visible").hide();
 	});
+	$('.thumbnail').mouseleave(function() {
+		    $(this).find(".hidden_action").hide();
+		    $(this).find(".visible").fadeToggle();
+		});
 
 });
